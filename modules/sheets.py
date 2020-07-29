f056c74590b8d94a65c991dd2e731bbe4baf6fa9
Chewyfitz <chewyfitz@gmail.com>
2020-07-29 17:20:21 +1000

    Revert "Revert "updating config paths for google sheets""
    This reverts commit 22cfb8f6cc317980b3423341b5511b6a4a389fa5.

--- modules/sheets.py
+++ modules/sheets.py
@@ -24,8 +24,8 @@ class GoogleSheet():
     
     def startService(self):
         creds = None
-        if os.path.exists('../token.pickle'):
-            with open('../token.pickle', 'rb') as token:
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
                 creds = pickle.load(token)
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
@@ -33,10 +33,10 @@ class GoogleSheet():
                 creds.refresh(Request())
             else:
                 flow = InstalledAppFlow.from_client_secrets_file(
-                    '../credentials.json', SCOPES)
+                    'credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
             # Save the credentials for the next run
-            with open('../token.pickle', 'wb') as token:
+            with open('token.pickle', 'wb') as token:
                 pickle.dump(creds, token)
         
         self.service = build('sheets', 'v4', credentials=creds)
